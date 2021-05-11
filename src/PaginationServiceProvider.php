1c62c86ef47084417fb644c242b59672b94469f7
Jordy Manner <jordy.manner@milkcreation.fr>
2021-05-11 14:43:17 +0000

    pagination >> 1.0.x-dev

--- src/PaginationServiceProvider.php
+++ src/PaginationServiceProvider.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Pollen\Pagination;
 
-use Pollen\Container\BaseServiceProvider;
+use Pollen\Container\BootableServiceProvider;
 use Pollen\Pagination\Adapters\WpPaginationAdapter;
 use Pollen\Pagination\Partial\PaginationPartial;
 use Pollen\Partial\PartialManagerInterface;
 
-class PaginationServiceProvider extends BaseServiceProvider
+class PaginationServiceProvider extends BootableServiceProvider
 {
     /**
      * @var string[]
